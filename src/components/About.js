8f3d479b7b5ab20a5d6c04480da6544d6365da0c
UzomaAkpala <francisakpala@yahoo.com>
2023-07-31 21:45:51 +0100

    modal update

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,24 @@
 import React from "react";
+import "./Modal.css";
 
 const About = () => {
   return (
-    <div
-      className="ui raised very padded text container segment"
-      style={{ marginTop: "80px" }}
-    >
-      <h3 className="ui header">About</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+    <div>
+      <div className="modal">
+        <div className="modal-content">
+          <h3>I am a Modal</h3>
+        </div>
+      </div>
+      <div
+        className="ui raised very padded text container segment"
+        style={{ marginTop: "80px" }}
+      >
+        <h3 className="ui header">About</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Modal from "./Modal";
 
 const Contact = () => {
   return (
-    <div
-      className="ui raised very padded text container segment"
-      style={{ marginTop: "80px" }}
-    >
-      <h3 className="ui header">Contact</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+    <div>
+      <Modal />
+      <div
+        className="ui raised very padded text container segment"
+        style={{ marginTop: "80px" }}
+      >
+        <h3 className="ui header">Contact</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
     </div>
   );
 };
