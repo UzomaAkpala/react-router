5b25cf6a3aad3b8645ea45e11ab760920c8b036e
UzomaAkpala <francisakpala@yahoo.com>
2023-07-31 15:33:13 +0100

    new updates

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      className="ui raised very padded text container segment"
+      style={{ marginTop: "80px" }}
+    >
+      <h3 className="ui header">About</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+};
+export default About;
